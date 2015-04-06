c95d6fdff63a8aab2313e0b5f2499eaec996968b
Jared Ramey <rameyjared@gmail.com>
2015-04-06 15:57:55 -0700

    Walkthrough complete

---
+++ WpfApplication1/WpfApplication1/ArrayClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class ArrayClass
+    {
+        public void ArrayExample()
+        {
+            int[] array = new int[] {0, 1, 2, 5, 7, 8, 11};
+
+            foreach(int i in array)
+            {
+                
+            }
+        }
+    }
+}
